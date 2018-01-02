c463fc08e775e07f90e12b73e4efea500c2bf855
佐藤大介 <workshify56@gmail.com>
2018-01-02 23:29:01 +0900

    AddConfigurePermittedParameters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      added_attrs = [ :username, :email, :password, :password_confirmation ]
+      devise_paramater_sanitizer.permit :sign_up, keys: added_attrs
+      devise_paramater_sanitizer.permit :account_update, keys: added_attrs
+      devise_paramater_sanitizer.permit :sign_in, keys: added_attrs
+    end
 end
