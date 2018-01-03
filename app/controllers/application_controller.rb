21b2caa6c8a6fcb33e32c25004e1d7e41e1eab78
佐藤大介 <workshify56@gmail.com>
2018-01-03 13:49:59 +0900

    AddFlashMessageFunction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   private
     def configure_permitted_parameters
       added_attrs = [ :username, :email, :password, :password_confirmation ]
-      devise_paramater_sanitizer.permit :sign_up, keys: added_attrs
-      devise_paramater_sanitizer.permit :account_update, keys: added_attrs
-      devise_paramater_sanitizer.permit :sign_in, keys: added_attrs
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+      devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     end
 end
