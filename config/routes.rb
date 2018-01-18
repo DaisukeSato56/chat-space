9d6c35d8563146e6357e2299bcc1ea127dcf4029
佐藤大介 <workshify56@gmail.com>
2018-01-18 16:29:03 +0900

    EditRoutes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
-  resources :users, only: [:edit, :update]
-  resources :groups, only: [:edit, :update, :create, :new] do
-    resources :messages, only: [:index, :create]
+  root 'groups#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:index, :edit, :update, :create, :new] do
+    resources :messages, only: [:index]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
