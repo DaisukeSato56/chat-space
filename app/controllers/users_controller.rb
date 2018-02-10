80072ac4cc0f9c6411a7775e560c4524930c13e5
佐藤大介 <workshify56@gmail.com>
2018-02-10 11:43:33 +0900

    ユーザー選択の際のインクリメンタルサーチ機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
+  def index
+    # @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: params[:user_ids]).limit(20)
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json{ render 'index', json: @users }
+    end
+  end
+
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
     if current_user.update(user_params)
-      redirect_to root_path
+      redirect_to root_path, notice: "変更が完了しました"
     else
       render :edit
     end
