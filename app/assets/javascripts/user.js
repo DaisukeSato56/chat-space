69926657d919810f4a68d64c283bfc6589bf9ead
佐藤大介 <workshify56@gmail.com>
2018-02-12 17:06:42 +0900

    ユーザーをチャットメンバーに追加した時にそのユーザーを削除

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,7 @@
 $(document).on('turbolinks:load', function(){
 
   var search_list = $('#user-search-result');
+  var users_list = $('#chat-group-users')
 
   function appendUser(user){
     var html = `<div class='chat-group-user clearfix'>
@@ -8,6 +9,17 @@ $(document).on('turbolinks:load', function(){
                   <a class='user-search-add chat-group-user__btn chat-group-user__btn--add' data-user-id='${ user.id }' data-user-name='${ user.name }'>追加</a>
                 </div>`
     search_list.append(html);
+  }
+
+  function appendChatMember(userId, UserName) {
+    var html = `
+                <div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${ userName }'>
+                  <p class='chat-group-user__name'>${ userName }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>
+                `
+    users_list.append(html);
   }
 
   $('#user-search-field').on('keyup', function(e){
@@ -36,6 +48,8 @@ $(document).on('turbolinks:load', function(){
 
   $(document).on("click", '.chat-group-user__btn--add', function(){
     $(this).parent().remove();
-
+    userId = $(this).attr('data-user-id')
+    userName = $(this).attr('data-user-name')
+    appendChatMember(userId, userName)
   });
 });
